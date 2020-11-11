39808954687cb50101a3196b7a89322b9c55ec9c
AlexMuzzy <alexmuzzy33@gmail.com>
2020-11-11 19:27:14 +0000

    Added javadocs.

--- src/AnagramMapper.java
+++ src/AnagramMapper.java
@@ -5,8 +5,25 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+
+/**
+ * AnagramMapper class. Extends Hadoop's Mapper class.
+ */
 public class AnagramMapper extends Mapper<Object, Text, Text, Text> {
 
+    /**
+     * Mapper method.
+     *
+     * Takes the given dataset and sorts each key alphabetically.
+     *
+     * Given sorted key and value is written to the given context.
+     *
+     * @param key Given key.
+     * @param value Given value.
+     * @param context Context to running job.
+     * @throws IOException Attempts to write given result.
+     * @throws InterruptedException Attempts to write given result.
+     */
     public void map(Object key, Text value, Context context)
             throws IOException, InterruptedException {
 
@@ -20,6 +37,11 @@ public class AnagramMapper extends Mapper<Object, Text, Text, Text> {
         }
     }
 
+    /**
+     * Alphabetically sort word and return the result.
+     * @param word Character array of word.
+     * @return sorted character array.
+     */
     private String SortGivenWord (char[] word){
 
         Arrays.sort(word);
--- src/AnagramReducer.java
+++ src/AnagramReducer.java
@@ -4,10 +4,24 @@ import org.apache.hadoop.mapreduce.Reducer;
 import java.io.IOException;
 import java.util.Optional;
 
+/**
+ * Anagram Reducer Class. Extends Hadoop's reducer class.
+ */
 public class AnagramReducer extends Reducer<Text, Text, Text, Text> {
 
+    /**
+     * Reducer method.
+     * Takes each key value pair and summarises every value to each distinct key.
+     *
+     * @param key Each given key.
+     * @param values Every given value.
+     * @param context Context of running job.
+     * @throws IOException Attempts to write given result.
+     * @throws InterruptedException Attempts to write given result.
+     */
     public void reduce(Text key, Iterable<Text> values, Context context)
             throws IOException, InterruptedException {
+
         StringBuilder anagram = null;
         for (Text val : values) {
             if (anagram == null) {
