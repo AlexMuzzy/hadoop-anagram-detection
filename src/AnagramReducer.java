cc479ee08cb0146f18d803eea29cb778a6f7a61c
AlexMuzzy <alexmuzzy33@gmail.com>
2020-11-17 19:52:48 +0000

    Change input to composite key.

--- src/AnagramReducer.java
+++ src/AnagramReducer.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 /**
  * Anagram Reducer Class. Extends Hadoop's reducer class.
  */
-public class AnagramReducer extends Reducer<Text, Text, Text, Text> {
+public class AnagramReducer extends Reducer<AnagramCompositeKey, Text, Integer, Text> {
 
     /**
      * Reducer method.
@@ -19,20 +19,28 @@ public class AnagramReducer extends Reducer<Text, Text, Text, Text> {
      * @throws IOException Attempts to write given result.
      * @throws InterruptedException Attempts to write given result.
      */
-    public void reduce(Text key, Iterable<Text> values, Context context)
+    public void reduce(AnagramCompositeKey key, Iterable<Text> values, Context context)
             throws IOException, InterruptedException {
 
         StringBuilder anagram = null;
+
         for (Text val : values) {
             if (anagram == null) {
                 anagram = Optional.ofNullable(val.toString()).map(StringBuilder::new).orElse(null);
             } else {
                 anagram.append(", ").append(val.toString());
             }
+            key.incrementFrequency();
         }
-        context.write(key, new Text(anagram == null ? null : anagram.toString()));
 
+        if (key.getFrequency() > 1) {//Check that there are more than one values to display the anagram.
+            assert anagram != null;
+            context.write(key.getFrequency(), FormatAnagram(key.getKeyName(), anagram));
+        }
+    }
 
+    private Text FormatAnagram(Text key, StringBuilder anagramSet) {
+        return new Text(key + " { " + anagramSet.toString() + " }");
     }
 }
 
