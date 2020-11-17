9bb8cbc0821b8391082badb805f59bf3844f8ef9
AlexMuzzy <alexmuzzy33@gmail.com>
2020-11-17 19:52:43 +0000

    Change output to composite key.

--- src/AnagramMapper.java
+++ src/AnagramMapper.java
@@ -1,3 +1,4 @@
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
@@ -9,14 +10,14 @@ import java.util.StringTokenizer;
 /**
  * AnagramMapper class. Extends Hadoop's Mapper class.
  */
-public class AnagramMapper extends Mapper<Object, Text, Text, Text> {
+public class AnagramMapper extends Mapper<Object, Text, AnagramCompositeKey, Text> {
 
     /**
      * Mapper method.
      *
      * Takes the given dataset and sorts each key alphabetically.
      *
-     * Given sorted key and value is written to the given context.
+     * Sorted key and value is written to the given context.
      *
      * @param key Given key.
      * @param value Given value.
@@ -32,20 +33,19 @@ public class AnagramMapper extends Mapper<Object, Text, Text, Text> {
         while (itr.hasMoreTokens()) {
             String currentWord = itr.nextToken();
             context.write(
-                    new Text(SortGivenWord(currentWord.toCharArray())),
+                    new AnagramCompositeKey(SortGivenWord(currentWord.toCharArray())),
                     new Text(currentWord));
         }
     }
 
     /**
      * Alphabetically sort word and return the result.
-     * @param word Character array of word.
-     * @return sorted character array.
+     * @param word Character array of word to be sorted.
+     * @return sorted character array as object Text.
      */
-    private String SortGivenWord (char[] word){
-
+    private Text SortGivenWord (char[] word){
         Arrays.sort(word);
-        return Arrays.toString(word);
+        return new Text(String.valueOf(word));
     }
 }
 
