55e3cb0b26f8dcd107dbfb8d7674644de8ddb5b5
Alex Muzzy <coarm@lunet.lboro.ac.uk>
2020-11-20 00:06:57 +0000

    Finished Composite Key impl.

--- src/AnagramCompositeKey.java
+++ src/AnagramCompositeKey.java
@@ -1,29 +1,58 @@
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.shaded.org.apache.curator.shaded.com.google.common.collect.ComparisonChain;
 
-public class AnagramCompositeKey {
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class AnagramCompositeKey implements Writable, WritableComparable<AnagramCompositeKey> {
 
     public IntWritable frequency;
     public Text keyName;
 
-    public AnagramCompositeKey(Text keyName) {
+    public AnagramCompositeKey() {
+        this.frequency = new IntWritable();
+        this.keyName = new Text();
+    }
+
+    public AnagramCompositeKey(Text keyName, IntWritable frequency) {
+        this.frequency = frequency;
         this.keyName = keyName;
-        this.frequency = new IntWritable(1);
     }
 
-    public int getFrequency() {
-        return frequency.get();
+    public IntWritable getFrequency() {
+        return frequency;
     }
 
-    public void incrementFrequency() {
-        this.frequency.set(this.frequency.get() + 1);
+    public void setFrequency(IntWritable frequency) {
+        this.frequency = frequency;
     }
 
     public Text getKeyName() {
-        return keyName;
+        return this.keyName;
     }
 
     public void setKeyName(Text keyName) {
         this.keyName = keyName;
+    }
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+        dataOutput.writeInt(this.getFrequency().get());
+        dataOutput.writeUTF(String.valueOf(this.keyName));
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+        this.setFrequency(new IntWritable(dataInput.readInt()));
+        this.setKeyName(new Text(dataInput.readUTF()));
+    }
+
+    @Override
+    public int compareTo(AnagramCompositeKey pair) {
+        return ComparisonChain.start().compare(this.frequency, pair.frequency).compare(this.keyName, pair.keyName).result();
     }
 }
