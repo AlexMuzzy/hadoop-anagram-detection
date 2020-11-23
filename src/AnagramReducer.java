f0f5c355bbaad5e77e6b6a0ae62c97e5172af92f
Alex Muzzy <coarm@lunet.lboro.ac.uk>
2020-11-23 23:50:18 +0000

    Updated reducer to make use of Composite value class.

--- src/AnagramReducer.java
+++ src/AnagramReducer.java
@@ -2,13 +2,24 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.util.TreeMap;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 /**
  * Anagram Reducer Class. Extends Hadoop's reducer class.
  */
 public class AnagramReducer extends Reducer<AnagramCompositeKey, Text, Text, Text> {
 
+
+
+    private TreeMap<AnagramCompositeKey, String> anagramMap;
+
+    @Override
+    public void setup(Context context) {
+        anagramMap = new TreeMap<>();
+    }
     /**
      * Reducer method.
      * Takes each key value pair and summarises every value to each distinct key.
@@ -16,33 +27,30 @@ public class AnagramReducer extends Reducer<AnagramCompositeKey, Text, Text, Tex
      * @param key Each given key.
      * @param values Every given value.
      * @param context Context of running job.
-     * @throws IOException Attempts to write given result.
-     * @throws InterruptedException Attempts to write given result.
      */
-    public void reduce(AnagramCompositeKey key, Iterable<Text> values, Context context)
-            throws IOException, InterruptedException {
+    public void reduce(AnagramCompositeKey key, Iterable<Text> values, Context context) {
 
-        StringBuilder anagram = null;
-        int endCount = 0;
-        for (Text val : values) {
-            if (anagram == null) {
-                anagram = Optional.ofNullable(val.toString()).map(StringBuilder::new).orElse(null);
-            } else {
-                anagram.append(", ").append(val.toString());
+        AnagramCompositeValues anagram = new AnagramCompositeValues(values);
+
+        if (anagram.getSize() > 1) {//Check that there are more than one values to display the anagram.
+            anagramMap.put(new AnagramCompositeKey(
+                    key.getKeyName(),
+                    key.getFrequency()),
+                    anagram.printWordCounts());
+        }
+    }
+
+    public void cleanup(Context context) {
+        anagramMap.forEach((key, value) -> {
+            try {
+                context.write(key.getKeyPair(), new Text(" { " + value + " } "));
+
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
             }
-            endCount += key.getFrequency().get();
-        }
-
-        if (key.getFrequency().get() > 1) {//Check that there are more than one values to display the anagram.
-            assert anagram != null;
-            context.write(new Text(
-                    key.getKeyName().toString() + " " + endCount),
-                    FormatAnagram(anagram));
-        }
+        });
     }
 
-    private Text FormatAnagram(StringBuilder anagramSet) {
-        return new Text(" { " + anagramSet.toString() + " }");
-    }
+
 }
 
