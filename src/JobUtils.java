9aa79150752794aa1e268d7fdc167ca72de54669
Alex Muzzy <coarm@lunet.lboro.ac.uk>
2020-11-23 23:48:59 +0000

    Added stop words request method.

--- src/JobUtils.java
+++ src/JobUtils.java
@@ -5,6 +5,11 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 
 import java.io.IOException;
+import java.io.InputStream;
+
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -38,13 +43,33 @@ public class JobUtils {
         String userInputString = scan.nextLine();
         scan.close();
 
-        if (userInputString.isEmpty()){
+        if (userInputString.isEmpty()) {
             hdfs.delete(outputPath, true);// Delete the given folder.
         }
 
         return true;
     }
 
+    /**
+     * performs a HTML GET request to the given stop word list and forwards the data
+     * to a list of strings.
+     * @return List of strings of the given stop words.
+     * @throws IOException Throws Exception when IO operation is performed.
+     */
+    public static String[] requestAndSaveStopWords() throws IOException {
 
+        URL urlString = new URL("https://www.textfixer.com/tutorials/common-english-words-with-contractions.txt");
+
+        StringBuilder stopWords = new StringBuilder();
+
+        // open the url stream with UTF-8 encoding.
+        try (Scanner scanner = new Scanner(urlString.openStream(), StandardCharsets.UTF_8.toString())) {
+
+            scanner.useDelimiter("\\A"); //RegEx pattern to begin at start of string.
+
+            return stopWords.append(scanner.hasNext() ? scanner.next() : "")
+                    .toString().split(","); //return string array of given stop words.
+        }
+    }
 
 }
