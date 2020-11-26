fb0654bbf6c6f683693adebb9a07cae60f1c9ff0
AlexMuzzy <alexmuzzy33@gmail.com>
2020-11-26 02:31:05 +0000

    - Code formatting and comments. - Moved try catch stop words to its own private method; getStopWords.

--- src/AnagramMapper.java
+++ src/AnagramMapper.java
@@ -14,44 +14,48 @@ import java.util.StringTokenizer;
  */
 public class AnagramMapper extends Mapper<Object, Text, AnagramCompositeKey, Text> {
 
-    List<String> stopWords;
-
-    { //TODO: wrap inside private method.
-        try {
-            stopWords = Arrays.asList(JobUtils.requestAndSaveStopWords());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+    List<String> stopWords = getStopWords();
 
     /**
      * Mapper method.
-     *
+     * <p>
      * Takes the given dataset and sorts each key alphabetically.
-     *
+     * <p>
      * Sorted key and value is written to the given context.
      *
-     * @param key Given key.
-     * @param value Given value.
+     * @param key     Given key.
+     * @param value   Given value.
      * @param context Context to running job.
-     * @throws IOException Attempts to write given result.
+     * @throws IOException          Attempts to write given result.
      * @throws InterruptedException Attempts to write given result.
      */
     public void map(Object key, Text value, Context context)
             throws IOException, InterruptedException {
 
+        //Splits string into whitespace separated tokens for iteration.
         StringTokenizer itr = new StringTokenizer(value.toString());
 
         while (itr.hasMoreTokens()) {
-
+            //Uses a regular expression to remove:
+            //
+            // - All apostrophes that exist are the beginning
+            //   and at the end of the word in a string are removed.
+            //
+            // - NOT all characters in a word along with NOT
+            //   all apostrophes in a word. Meaning "you'll" is valid
+            //   but "test'" will convert to "test".
+            //
+            // Word is even converted to lowercase.
             String currentWord = itr.nextToken()
                     .replaceAll("(^')|('$)|[^'a-zA-Z]", "")
                     .toLowerCase();
 
+            // If stop word is found, don't write to context.
             if (stopWords.contains(currentWord)) continue;
+
             AnagramCompositeKey reducerKey = new AnagramCompositeKey(
-                            SortGivenWord(currentWord.toCharArray()),
-                            new IntWritable(1));
+                    SortGivenWord(currentWord.toCharArray()),
+                    new IntWritable(1));
 
             context.write(
                     reducerKey,
@@ -61,12 +65,27 @@ public class AnagramMapper extends Mapper<Object, Text, AnagramCompositeKey, Tex
 
     /**
      * Alphabetically sort word and return the result.
+     *
      * @param word Character array of word to be sorted.
      * @return sorted character array as object Text.
      */
-    private Text SortGivenWord (char[] word){
+    private Text SortGivenWord(char[] word) {
         Arrays.sort(word);
         return new Text(new String(word));
+    }
+
+    /**
+     * Gets stop words for filtering input data.
+     *
+     * @return List of stop words.
+     */
+    private List<String> getStopWords() {
+        try {
+            return stopWords = Arrays.asList(JobUtils.requestAndSaveStopWords());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
 
